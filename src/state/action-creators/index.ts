e5ee6963ed5725dd585bf2db2ada87160b762b09
Chaewon <fromnowwon@gmail.com>
2024-02-08 09:53:43 +0900

    Typing Action Creators

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,30 @@
+import { ActionType } from "../action-types"
+import { Dispatch } from "redux";
+import { Action } from "../actions"
+
+export const depositMoney = (amount: number) => {
+  // dispatch가 무엇을 디스패치할지 명시
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.DEPOSIT,
+      payload: amount,
+    })
+  }
+}
+
+export const withdrawMoney = (amount: number) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.WITHDRAW,
+      payload: amount,
+    })
+  }
+}
+
+export const bankruptMoney = (amount: number) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BANKRUPT,
+    })
+  }
+}
